683d73cb8a538d04f561c8fcd9907f9cb64d7ed6
John Doe <johndoe@example.com>
2018-12-15 10:52:33 +0100

    Commit First Commit Master

---
+++ logistic_regression.py
@@ -0,0 +1,7 @@
+# implemenent logistic_regression
+# for multiclass problem of recognizing hand gesture by muscular acitivity
+
+# theano implemenented functions that we use here are softmax, sigmoid, relu
+
+import numpy as np
+import
