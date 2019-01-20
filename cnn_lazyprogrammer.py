7aa12322ec6b4ffbe05f9e4c2926a0df8d922393
John Doe <johndoe@example.com>
2019-01-20 12:20:19 +0100

    Continue Implement lazy_cnn

--- cnn_lazyprogrammer.py
+++ cnn_lazyprogrammer.py
@@ -63,5 +63,54 @@ def main():
     n_batches = N // batch_sz
 
     # input size into the graph shall be constant
+    # limit samples since input will always have to be same size
+    # you could also just do N = N / batch_sz * batch_sz
+    Xtrain = Xtrain[:73000, ]
+    Ytrain = Ytrain[:73000]
+    Xtest = Xtest[:26000, ]
+    Ytest = Ytest[:26000]
+    # print "Xtest.shape:", Xtest.shape
+    # print "Ytest.shape:", Ytest.shape
 
-    Xtrain
+    # initial weights
+    M = 500
+    K = 10
+    poolsz = (2, 2)
+
+    # (filter_width, filter_height, num_color_channels, num_feature_maps)
+    W1_shape = (5, 5, 3, 20)
+    W1_init = init_filter(W1_shape, poolsz)
+    # one bias per output feature map
+    b1_init = np.zeros(W1_shape[-1], dtype=np.float32)
+
+    # (filter_width, filter_height, old_num_feature_maps, num_feature_maps)
+    W2_shape = (5, 5, 20, 50)
+    W2_init = init_filter(W2_shape, poolsz)
+    b2_init = np.zeros(W2_shape[-1], dtype=np.float32)
+
+    # vanilla ANN weights
+    W3_init = np.random.randn(
+        W2_shape[-1]*8*8, M) / np.sqrt(W2_shape[-1]*8*8 + M)
+    b3_init = np.zeros(M, dtype=np.float32)
+    W4_init = np.random.randn(M, K) / np.sqrt(M + K)
+    b4_init = np.zeros(K, dtype=np.float32)
+
+    # define variables and expressions
+    # using None as the first shape element takes up too much RAM unfortunately
+    X = tf.placeholder(tf.float32, shape=(batch_sz, 32, 32, 3), name='X')
+    T = tf.placeholder(tf.int32, shape=(batch_sz,), name='T')
+    W1 = tf.Variable(W1_init.astype(np.float32))
+    b1 = tf.Variable(b1_init.astype(np.float32))
+    W2 = tf.Variable(W2_init.astype(np.float32))
+    b2 = tf.Variable(b2_init.astype(np.float32))
+    W3 = tf.Variable(W3_init.astype(np.float32))
+    b3 = tf.Variable(b3_init.astype(np.float32))
+    W4 = tf.Variable(W4_init.astype(np.float32))
+    b4 = tf.Variable(b4_init.astype(np.float32))
+
+    Z1 = convpool(X, W1, b1)
+    Z2 = convpool(Z1, W2, b2)
+    Z2_shape = Z2.get_shape().as_list()
+    Z2r = tf.reshape(Z2, [Z2_shape[0], np.prod(Z2_shape[1:])])
+    Z3 = tf.nn.relu(tf.matmul(Z2r, W3) + b3)
+    Yish = tf.matmul(Z3, W4) + b4
